3bade704bf1858e7ab1979a98901e86c2eebaf0c
Ryan Greenlee <ryan.greenlee1@gmail.com>
2018-04-20 23:01:59 -0400

    Started trip class

---
+++ Rosuber/Trip.swift
@@ -0,0 +1,43 @@
+//
+//  Trip.swift
+//  Rosuber
+//
+//  Created by Ryan Greenlee on 4/20/18.
+//  Copyright © 2018 FengYizhi. All rights reserved.
+//
+
+import UIKit
+
+class Trip: NSObject {
+    var id: String?
+    var capacity: Int
+    var destination: String
+//    var driverKey: String
+    var origin: String
+    var price: Float
+    var time: Date
+    
+    let capacityKey = "capacity"
+    let destinationKey = "destination"
+//    let driverKeyKey = "driverKey"
+    let originKey = "origin"
+    let priceKey = "price"
+    let timeKey = "time"
+    
+    init(capacity: Int, destination: String, origin: String, price: Float, time: Date) {
+        self.capacity = capacity
+        self.destination = destination
+        self.origin = origin
+        self.price = price
+        self.time = time
+    }
+    
+    var data: [String: Any] {
+        return [capacityKey: self.capacity,
+                destinationKey: self.destination,
+                originKey: self.origin,
+                priceKey: self.price,
+                timeKey: self.time]
+    }
+
+}
