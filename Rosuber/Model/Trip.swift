60d631d24b38b80aedb911f39b723e3361fe4c51
Ryan Greenlee <ryan.greenlee1@gmail.com>
2018-04-27 12:30:05 -0400

    Started adding firebase for FindTripsViewController.

--- Rosuber/Model/Trip.swift
+++ Rosuber/Model/Trip.swift
@@ -6,13 +6,14 @@
 //  Copyright © 2018 FengYizhi. All rights reserved.
 //
 import UIKit
+import Firebase
 
 class Trip: NSObject {
     var id: String?
     var capacity: Int
     var destination: String
-    var driverKey: String?
-    var passengerKeys: String?
+    var driverKey: String
+    var passengerKeys: String
     var origin: String
     var price: Float
     var time: Date
@@ -28,14 +29,30 @@ class Trip: NSObject {
     init(capacity: Int, destination: String, origin: String, price: Float, time: Date) {
         self.capacity = capacity
         self.destination = destination
+        self.driverKey = ""
+        self.passengerKeys = ""
         self.origin = origin
         self.price = price
         self.time = time
     }
     
+    init(documentSnapshot: DocumentSnapshot) {
+        self.id = documentSnapshot.documentID
+        let data = documentSnapshot.data()!
+        self.capacity = data[capacityKey] as! Int
+        self.destination = data[destinationKey] as! String
+        self.driverKey = data[driverKeyKey] as! String
+        self.passengerKeys = data[passengerKeysKey] as! String
+        self.origin = data[originKey] as! String
+        self.price = data[priceKey] as! Float
+        self.time = data[timeKey] as! Date
+    }
+    
     var data: [String: Any] {
         return [capacityKey: self.capacity,
                 destinationKey: self.destination,
+                driverKeyKey: self.driverKey,
+                passengerKeysKey: self.passengerKeys,
                 originKey: self.origin,
                 priceKey: self.price,
                 timeKey: self.time]
